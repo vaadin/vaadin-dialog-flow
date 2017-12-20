591ba75ede771b0661541cb850cb8210dc93f671
Pekka Maanpaa <pekkamaa@vaadin.com>
2017-12-20 11:02:19 +0200

    Add support for components inside Dialog

--- src/main/java/com/vaadin/flow/component/dialog/Dialog.java
+++ src/main/java/com/vaadin/flow/component/dialog/Dialog.java
@@ -15,47 +15,84 @@
  */
 package com.vaadin.flow.component.dialog;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.server.VaadinRequest;
 
 /**
  * Server-side component for the {@code <vaadin-dialog>} element.
  * 
  * @author Vaadin Ltd
  */
-public class Dialog extends GeneratedVaadinDialog<Dialog> {
+@HtmlImport("bower_components/polymer/polymer.html")
+@HtmlImport("frontend://flow-component-renderer.html")
+public class Dialog extends GeneratedVaadinDialog<Dialog>
+        implements HasComponents {
+
+    private Element container;
 
     /**
      * Creates an empty dialog.
      */
     public Dialog() {
-        this("");
+        container = new Element("div", false);
+        getElement().appendVirtualChild(container);
+
+        getElement().getNode().runWhenAttached(ui -> {
+            String appId = UI.getCurrent().getSession().getService()
+                    .getMainDivId(UI.getCurrent().getSession(),
+                            VaadinRequest.getCurrent());
+            appId = appId.substring(0, appId.indexOf("-"));
+
+            int nodeId = container.getNode().getId();
+
+            String template = "<template><flow-component-renderer appid="
+                    + appId + " nodeid=" + nodeId
+                    + "></flow-component-renderer></template>";
+            getElement().setProperty("innerHTML", template);
+        });
     }
 
     /**
-     * Creates a dialog with the given String rendered as it's HTML content.
+     * Creates a dialog with given components inside.
      * 
-     * @param content
-     *            the content of the Dialog as HTML markup
+     * @param components
+     *            the components inside the dialog
+     * @see #add(Component...)
      */
-    public Dialog(String content) {
-        Element templateElement = new Element("template");
-        getElement().appendChild(templateElement);
-
-        templateElement.setProperty("innerHTML", content);
+    public Dialog(Component... components) {
+        this();
+        add(components);
     }
 
-    /**
-     * Opens the dialog.
-     */
-    public void open() {
-        setOpened(true);
+    @Override
+    public void add(Component... components) {
+        assert components != null;
+        for (Component component : components) {
+            assert component != null;
+            container.appendChild(component.getElement());
+        }
     }
 
-    /**
-     * Closes the dialog.
-     */
-    public void close() {
-        setOpened(false);
+    @Override
+    public void remove(Component... components) {
+        for (Component component : components) {
+            assert component != null;
+            if (container.equals(component.getElement().getParent())) {
+                container.removeChild(component.getElement());
+            } else {
+                throw new IllegalArgumentException("The given component ("
+                        + component + ") is not a child of this component");
+            }
+        }
+    }
+
+    @Override
+    public void removeAll() {
+        container.removeAllChildren();
     }
 
     /**
@@ -106,6 +143,31 @@ public class Dialog extends GeneratedVaadinDialog<Dialog> {
      */
     public void setCloseOnOutsideClick(boolean closeOnOutsideClick) {
         getElement().setProperty("noCloseOnOutsideClick", !closeOnOutsideClick);
+    }
+
+    /**
+     * Opens the dialog.
+     */
+    public void open() {
+        setOpened(true);
+    }
+
+    /**
+     * Closes the dialog.
+     */
+    public void close() {
+        setOpened(false);
+    }
+
+    @Override
+    public void setOpened(boolean opened) {
+        if (opened && !getElement().getNode().isAttached()
+                && UI.getCurrent() != null) {
+            // Add the element to body when it's opened,
+            // if it hasn't been attached already somewhere.
+            UI.getCurrent().add(this);
+        }
+        super.setOpened(opened);
     }
 
 }
