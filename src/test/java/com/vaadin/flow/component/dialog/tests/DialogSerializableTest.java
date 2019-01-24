8e0d8dddbb9b01676c1d0004d2cee628412fb62c
Joni Uitto <joni@vaadin.com>
2019-01-24 17:53:22 +0200

    Refactoring: created submodules

--- src/test/java/com/vaadin/flow/component/dialog/tests/DialogSerializableTest.java
+++
@@ -1,6 +0,0 @@
-package com.vaadin.flow.component.dialog.tests;
-
-import com.vaadin.flow.testutil.ClassesSerializableTest;
-
-public class DialogSerializableTest extends ClassesSerializableTest {
-}
